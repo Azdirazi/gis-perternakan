333d8b3baeac32aaa2953ca9207e50db5595c3ef
Azdirazi <91419014+Azdirazi@users.noreply.github.com>
2023-05-12 12:26:43 +0700

    new

--- connection.example.php
+++ connection.example.php
@@ -1,10 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-<<<<<<< HEAD
 $connection = new mysqli ("localhost", "root", "", "db_peternakan");
-=======
-$connection = new mysqli ("localhost", "root", "", "db_absen");
->>>>>>> a310f9f1efd40fdc75347a01a1f0cfd9e82f288c
 
 if ($connection->connect_error != null) {
     echo  "Gagal terhubung ke Database";
